f2f7fe2d7217f22b12ee2d73525bcb053796c186
Isaac Dennis <isaacdennissombra@gmail.com>
2023-02-06 22:40:44 -0300

    Added comma example and exercise

---
+++ chapter03/exercises/exercise3-10.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func comma(str string) string {
+	s := str
+	for i := len(s); i > 3; i = i-3 {
+		s = s[:i-3] + "," + s[i-3:]
+	}
+	return s
+}
+
+func main() {
+	n1 := "12345678"
+	fmt.Printf("%s\n", comma(n1))
+}
