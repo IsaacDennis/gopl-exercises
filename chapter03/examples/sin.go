27a8ea9ddb691f2acabc7403c7fb39d24a2d8087
Isaac Dennis <isaacdennissombra@gmail.com>
2023-02-23 12:48:13 -0300

    Remove sin.go comments

--- chapter03/examples/sin.go
+++ chapter03/examples/sin.go
@@ -1,10 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 58.
-//!+
-
-// Surface computes an SVG rendering of a 3-D surface function.
 package main
 
 import (
@@ -14,11 +7,11 @@ import (
 
 const (
 	width, height = 600, 320            // canvas size in pixels
-	cells         = 100           // number of grid cells
+	cells         = 100                 // number of grid cells
 	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
 	xyscale       = width / 2 / xyrange // pixels per x or y unit
 	zscale        = height * 0.4        // pixels per z unit
-	angle         = math.Pi / 6        // angle of x, y axes (=30°)
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
@@ -41,14 +34,12 @@ func main() {
 }
 
 func corner(i, j int) (float64, float64) {
-	// Find point (x,y) at corner of cell (i,j).
+
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
-	// Compute surface height z.
-	z := f(x, y)
 
-	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	z := f(x, y)
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
 	return sx, sy
@@ -58,5 +49,3 @@ func f(x, y float64) float64 {
 	r := math.Hypot(x, y) // distance from (0,0)
 	return math.Sin(r) / r
 }
-
-//!-
