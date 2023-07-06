aa0ad218f064b67c43d680b0b7d15088cec94cfd
Isaac Dennis <isaacdennissombra@gmail.com>
2023-07-06 13:20:56 -0300

    Add Exercise 2.3 solution

---
+++ chapter02/exercises/exercise2-3.go
@@ -0,0 +1,18 @@
+package main
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// Devolve a população (número de bits definidos) de x
+func PopCount(x uint64) int {
+	var pop int
+	for i := 0; i < 8; i++ {
+		pop += int(pc[byte(x>>(i*8))])
+	}
+	return pop
+}
